f9511c00e4ce2fcee436f26151e0e69683fb694c
krrishnaingle2001.gmail.com <krishnaingle2001@gmail.com>
2025-07-30 21:38:06 +0530

    done

---
+++ MULTITHREADING_IN_JAVA/src/com/multithreading/itc/WhyITCisImportant.java
@@ -0,0 +1,50 @@
+package com.multithreading.itc;
+//WAP that describes, If we don't have proper communication between two threads then output  is un-predictable
+class Test implements Runnable
+{
+	private int data = 0;
+	@Override
+	public void run()
+	{
+		try {
+			   Thread.sleep(5);
+		}
+		catch(InterruptedException e) {
+			System.err.println(e);
+		}
+		
+		
+		
+		for(int i=1;i<=10;i++) {
+			data= data+i;
+		}
+		
+	}
+	
+	
+	
+	public int getData() {
+		return this.data;
+	}
+}
+
+
+
+public class WhyITCisImportant
+{
+	
+  public static void main(String[] args) throws InterruptedException 
+  {
+	System.out.println("Main Thread Started...!!");
+	
+	Test t1= new Test();
+	
+	
+	Thread thread = new Thread(t1,"Child Thread");
+	thread.start();
+	Thread.sleep(10);
+	System.out.println(t1.getData());
+	
+	System.out.println("Main Thread ended!!!");
+  }
+}
